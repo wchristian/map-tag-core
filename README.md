05529abb518b2558fee08c6e5e744854d9d2099d
Subject-31415 <168562337+Subject-314159@users.noreply.github.com>
2024-08-19 21:27:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Source code for [Map tag GPS](https://mods.factorio.com/mods/map-tag-gps) and [Map tag teleport](https://mods.factorio.com/mods/map-tag-teleport)
---
+++ data.lua
@@ -0,0 +1,30 @@
+data:extend({{
+    type = "custom-input",
+    name = "mt_give-gps-selection-tool",
+    key_sequence = "SHIFT + ALT + G",
+    action = "spawn-item",
+    item_to_spawn = "mt_gps-selection-tool",
+    order = "a"
+}, {
+    type = "shortcut",
+    name = "mt_give-gps-selection-tool",
+    icon = {
+        filename = "__map-tag-core__/graphics/icons/pin.png",
+        size = 64
+    },
+    action = "spawn-item",
+    item_to_spawn = "mt_gps-selection-tool"
+}, {
+    type = "selection-tool",
+    name = "mt_gps-selection-tool",
+    icon = "__map-tag-core__/graphics/icons/pin.png",
+    icon_size = 64,
+    stack_size = 1,
+    flags = {"only-in-cursor", "hidden", "spawnable", "not-stackable"},
+    selection_mode = "nothing",
+    alt_selection_mode = "nothing",
+    selection_color = {1, 1, 1},
+    alt_selection_color = {0, 0, 0, 0},
+    selection_cursor_box_type = "not-allowed",
+    alt_selection_cursor_box_type = "not-allowed"
+} --[[@as data.SelectionToolPrototype]] })
